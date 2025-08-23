d22b403152c2b953bde67cb65c2d1f1c22acd722
Robin Singh <vougishtiger@gmail.com>
2025-08-23 12:13:48 -0700

    update frontend

--- src/components/Newspaper.jsx
+++ src/components/Newspaper.jsx
@@ -4,6 +4,7 @@ import Skills from "./Skills";
 import Projects from "./Projects";
 import Contact from "./Contact";
 import Footer from "./Footer";
+import QuoteOfTheDay from "./QuoteOfTheDay";
 
 const Newspaper= ()=> {
   return (
